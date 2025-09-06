8f7f9ab03c3ad9bbb7cd389d3cf1b677b090182a
AmeyPawar <148186608+AmeyPawar@users.noreply.github.com>
2025-09-06 23:04:10 +0530

    Add files via upload

---
+++ Bill Generation/src/dbConnection.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ameyp
+ */
+public class dbConnection {
+     public static Connection con(){
+        Connection con=null;
+    try{
+       Class.forName("com.mysql.jdbc.Driver");
+       con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bill2","root","");
+  
+
+    
+    }catch(Exception e){
+        JOptionPane.showMessageDialog(null, e);
+
+    }
+    return con;
+    }
+    
+    
+}
